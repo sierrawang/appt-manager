cf1f707d3ddf2a721fc6153cd36d60a6723a4571
Sierra Wang <sierrawang97@gmail.com>
2017-06-29 20:38:07 +0000

    make homepage

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Appointment Manager"
+  end
+  
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+end
